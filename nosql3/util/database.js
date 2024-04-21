4bd52b5078c046a38974cc698f7bd4fca17b61c8
ruchitakb7 <ruchitakb7@gmail.com>
2024-04-22 04:43:23 +0530

    user table

---
+++ nosql3/util/database.js
@@ -0,0 +1,27 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://ruchitakb7:oi87W2nteIujbQsK@cluster0.7iolqg4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
